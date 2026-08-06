f6c38ac885a9d258808c49edba249b1d178b8090
ProfeAlbeiro <profealbeiro2020@gmail.com>
2026-08-05 20:55:11 -0500

    Constructores

--- controllers/Roles.php
+++ controllers/Roles.php
@@ -1,5 +1,4 @@
 <?php
-
 // Llamar la Clase Rol
 require_once "models/Rol.php";
 
@@ -7,13 +6,36 @@ class Roles {
 
     public function main(){
 
-        // Instanciar la Clase Rol (Crear Objeto)
-        $rol = new Rol;
-        
+        echo "MÉTODOS SETTER Y GETTER<br><br>";
+        // Obj01: Instanciar la Clase Rol (Crear Objeto)
+        $rol01 = new Rol;        
         // Código del Rol: Capturar y Mostrar 
-        $rol->setRolCode(05);
-        echo $rol->getRolCode();
+        $rol01->setRolCode(1);
+        $rol01->setRolName("admin");
+        echo "Id: " . $rol01->getRolCode() . "<br>";
+        echo "Nombre: " . $rol01->getRolName() . "<br><br>";
 
+        // Obj02: Instanciar la Clase Rol (Crear Objeto)
+        $rol02 = new Rol;        
+        // Código del Rol: Capturar y Mostrar 
+        $rol02->setRolCode(2);
+        $rol02->setRolName("vendedor");
+        echo "Id: " . $rol02->getRolCode() . "<br>";
+        echo "Nombre: " . $rol02->getRolName() . "<br><br>";
+        
+        // Obj03: Instanciar la Clase Rol (Crear Objeto)
+        $rol03 = new Rol;        
+        // Código del Rol: Capturar y Mostrar 
+        $rol03->setRolCode(3);
+        $rol03->setRolName("cliente");
+        echo "Id: " . $rol03->getRolCode() . "<br>";
+        echo "Nombre: " . $rol03->getRolName() . "<br><br>";
+        
+        // Obj04: Instanciar la Clase Rol (Crear Objeto)
+        echo "MÉTODO CONSTRUCTOR <br><br>";
+        $rol04 = new Rol(4, "Cajero");
+        echo "Id: " . $rol04->getRolCode() . "<br>";
+        echo "Nombre: " . $rol04->getRolName() . "<br><br>";
     }
 }
 ?>
--- models/Rol.php
+++ models/Rol.php
@@ -2,12 +2,35 @@
 
 class Rol {
 
-    // ATRIBUTOS 
+    //             ATRIBUTOS 
+    
     private $rolCode;
     private $rolName;
 
-    // MÉTODOS
+    //              MÉTODOS    
     
+    // Sobrecarga Constructores
+    public function __construct(){
+        try {
+            $a = func_get_args();
+            $i = func_num_args();
+            if (method_exists($this, $f = '__construct' . $i)) {
+                call_user_func_array(array($this, $f), $a);
+            }
+        } catch (Exception $e) {
+            die($e->getMessage());
+        }
+    }
+
+    # Constructor: Objeto 00 parámetros
+    public function __construct0(){}
+
+    # Constructor: Objeto 02 parámetros
+    public function __construct2($rolCode,$rolName){
+        $this->rolCode = $rolCode;
+        $this->rolName = $rolName;
+    }
+
     // Métodos Setter y Getter
 
     # Código Rol
@@ -16,6 +39,13 @@ class Rol {
     }
     public function getRolCode(){
         return $this->rolCode;
+    }
+    # Nombre Rol
+    public function setRolName($rolName){
+        $this->rolName = $rolName;
+    }
+    public function getRolName(){
+        return $this->rolName;
     }
 }
 ?>
